6baa27b20ab82cd33f6564a30553b431edc4f6da
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-02 02:04:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,10 @@ end
 
 def monopoly_with_second_tier
 monopoly = {}
-	monopoly={:railroads=>{":pieces"=> "4"}
-	  }
+	monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
 end
 
 def monopoly_with_third_tier
